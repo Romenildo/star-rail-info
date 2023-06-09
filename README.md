476244d3caf99c08c0ddafd7fc76ccc7545d9c11
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-06-09 18:58:17 -0300

    alterando entre os detalhes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Desenvolvimento
 
 link para testes: https://star-rail-info.vercel.app
+id de exemplo para testar: 601524235
--- app/components/character.tsx
+++ app/components/character.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from "react"
 import Detail from "./charInfo/detail"
 import LightConeDetail from "./charInfo/lightConeDetail"
 
@@ -6,7 +8,24 @@ interface CharacterProps{
     profileName?: string
 }
 
+
 const Character = (props: CharacterProps) =>{
+
+    const [detailSelected, setDetailSelected] = useState<string>("char")
+
+
+    const selectedDetailRender = ()=>{
+        if(detailSelected ==="char"){
+            return <Detail character={props.character} profileName={props.profileName}/>
+        }
+        if(detailSelected ==="lightCone"){
+            return <LightConeDetail lightCone={props.character?.light_cone}/>
+        }
+        if(detailSelected ==="relic"){
+            //return 
+        }
+    }
+
     return (
         <div className="w-full bg-gray-600 flex items-center justify-center pt-3">
             <div className=" bg-[url('/images/bg.png')] bg-cover w-11/12 pb-5 flex p-4 min-w-fit">
@@ -18,15 +37,21 @@ const Character = (props: CharacterProps) =>{
                     </div>
                     
                     <ul className="w-full flex flex-col justify-center items-start py-10  gap-8 text-xl relative left-14">
-                        <li className="hover:text-yellow-300 hover:cursor-pointer hover:bg-white hover:bg-opacity-5 px-10 py-2 rounded-3xl flex gap-2 pl-2">
+                        <li className="hover:text-yellow-300 hover:cursor-pointer hover:bg-white hover:bg-opacity-5 px-10 py-2 rounded-3xl flex gap-2 pl-2"
+                            onClick={()=>{setDetailSelected("char")}}
+                        >
                             <img className="w-[35px] h-[25px] pl-2" src={`https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/sign/NoviceAvatarIcon.png` } alt="Personagem" />
                             Detalhes
                             </li>
-                        <li className="hover:text-yellow-300 hover:cursor-pointer hover:bg-white hover:bg-opacity-5 px-10 py-2 rounded-3xl flex gap-2 pl-2">
+                        <li className="hover:text-yellow-300 hover:cursor-pointer hover:bg-white hover:bg-opacity-5 px-10 py-2 rounded-3xl flex gap-2 pl-2"
+                            onClick={()=>{setDetailSelected("lightCone")}}
+                        >
                             <img className="w-[35px] h-[25px] pl-2" src={`https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/sign/NoviceBookIcon.png` } alt="Personagem" />
                             Cone de luz
                             </li>
-                        <li className="hover:text-yellow-300 hover:cursor-pointer hover:bg-white hover:bg-opacity-5 px-10 py-2 rounded-3xl flex gap-2 pl-2">
+                        <li className="hover:text-yellow-300 hover:cursor-pointer hover:bg-white hover:bg-opacity-5 px-10 py-2 rounded-3xl flex gap-2 pl-2"
+                            onClick={()=>{setDetailSelected("relic")}}
+                        >
                             <img className="w-[35px] h-[25px] pl-2" src={`https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/sign/NoviceAchievementIcon.png` } alt="Personagem" />
                             Relíquias
                         </li>
@@ -34,8 +59,8 @@ const Character = (props: CharacterProps) =>{
                 </div>
 
                 <div className="pl-20">
-                    {/** <Detail character={props.character} profileName={props.profileName}/>*/}
-                    <LightConeDetail lightCone={props.character?.light_cone}/>
+                    {selectedDetailRender()}
+                    
                 </div>
 
             </div>
